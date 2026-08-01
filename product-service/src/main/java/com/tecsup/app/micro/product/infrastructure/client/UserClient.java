ca87d53a9fb4711d3432a6cd3e0ae16644035d4b
jgomezz <jgomezz@gmail.com>
2026-07-31 22:16:52 -0500

    update CircuitBreaker

--- product-service/src/main/java/com/tecsup/app/micro/product/infrastructure/client/UserClient.java
+++ product-service/src/main/java/com/tecsup/app/micro/product/infrastructure/client/UserClient.java
@@ -50,8 +50,8 @@ public class UserClient {
      *                    abre el circuito por 10 segundos
      *   @Retry: Reintenta hasta 3 veces con 1 segundo entre intentos
      */
-    @CircuitBreaker(name = "userService", fallbackMethod = "getUserFallback")
-    @Retry(name = "userService")
+    @CircuitBreaker(name = "userService")
+    @Retry(name = "userService", fallbackMethod = "getUserFallback")
     public User getUserById(Long userId, String jwtToken) {
         log.info("Calling User Service (PostgreSQL userdb) to get user with id: {}", userId);
 
